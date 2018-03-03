1c6b4888c41221f43d81de43eb8c6740435ccce4
Jan Ritter <jan-ritter@outlook.com>
2018-03-03 22:19:59 +0100

    feat: added function to get the geo-ip information as a struct for a provided IP

---
+++ geoip/geoip.go
@@ -0,0 +1,51 @@
+package geoip
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type IpGeoInfo struct {
+	IP          string  `json:"ip"`
+	CountryCode string  `json:"country_code"`
+	CountryName string  `json:"country_name"`
+	RegionCode  string  `json:"region_code"`
+	RegionName  string  `json:"region_name"`
+	City        string  `json:"city"`
+	ZipCode     string  `json:"zip_code"`
+	TimeZone    string  `json:"time_zone"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+	MetroCode   int     `json:"metro_code"`
+}
+
+func ForIp(ip string) IpGeoInfo {
+	url := "https://freegeoip.net/json/"+ip
+
+	httpClient := http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := httpClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	geoInfo := IpGeoInfo{}
+	jsonErr := json.Unmarshal(body, &geoInfo)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return geoInfo
+}
