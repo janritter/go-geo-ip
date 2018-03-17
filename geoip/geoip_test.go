a077c7c9f79b1bebab0313d63fa965fcfdb9eecb
Jan Ritter <jan-ritter@outlook.com>
2018-03-17 16:19:23 +0100

    test: adapted tests for error returning

--- geoip/geoip_test.go
+++ geoip/geoip_test.go
@@ -3,7 +3,11 @@ package geoip
 import "testing"
 
 func TestForIP(t *testing.T) {
-	geoInfo := ForIP("8.8.8.8")
+	geoInfo, err := ForIP("8.8.8.8")
+
+	if err != nil {
+		t.Error("Expected nil, got ", err)
+	}
 
 	if geoInfo.CountryName != "United States" {
 		t.Error("Expected Unites States, got ", geoInfo.CountryName)
@@ -15,7 +19,11 @@ func TestForIP(t *testing.T) {
 }
 
 func TestForDomain(t *testing.T) {
-	geoInfo := ForDomain("google.com")
+	geoInfo, err := ForDomain("google.com")
+
+	if err != nil {
+		t.Error("Expected nil, got ", err)
+	}
 
 	if geoInfo.CountryCode != "US" {
 		t.Error("expected US, got ", geoInfo.CountryCode)
